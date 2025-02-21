edff09b4273006f5fc64e74a969dd07fba885a20
dama1833 <52755541+dama1833@users.noreply.github.com>
2025-02-21 12:10:43 -0700

    update colmap

--- R/apply_col_map.R
+++ R/apply_col_map.R
@@ -21,15 +21,23 @@ apply_col_map <- function(df, convert_type = TRUE) {
     distname = "DDST_DISTRICT_NAME",
     schname = "DSCH_SCHOOL_NAME",
     sch  = "DSCH_SCHOOL_NUMBER",
+    grd_low = "DSCH_GRADE_SPAN_LOW",
+    grd_high = "DSCH_GRADE_SPAN_HIGH",
     test = "TEST_NAME",
     sub = "SUBJECT",
     emh = "DEMH_EMH_CODE",
     emh = "EMH_CODE",
     include = "INCLUDED_IN_CALCS_YN",
     aec = "ALT_ED_CAMPUS_YN",
+    aec = "AEC_YN",
+    charter = "CHATER_YN",
+    magnet = "MAGNET_YN",
+    innovation = "INNOVATION_YN",
+    online = "ONLINE_YN",
     mss = "ACH_MEAN_SS",
     mgp = "GRO_MEDIAN_SGP",
-    aec = "AEC_YN"
+    pct_pts = "PCT_PTS_EARN",
+    pct_pts_w = "PCT_PTS_EARN_WEIGHTED"
 
   )
 
@@ -53,8 +61,10 @@ apply_col_map <- function(df, convert_type = TRUE) {
   if (convert_type) {
     # Define standard variable names for conversion
     integer_vars <- c("scale_score", "sgp")
-    numeric_vars <- c("mss", "mgp")
-    factor_vars  <- c("year", "dist", "sch", "sub", "emh", "test")
+    numeric_vars <- c("mss", "mgp", "pct_pts", "pct_pts_w")
+    factor_vars  <- c("year", "dist", "sch", "sub", "emh", "test", "aec", "online",
+                      "charter", "magnet", "innovation", "grd_low", "grd_high",
+                      "rating")
 
     # Determine which of these variables exist in the dataframe
     integer_vars_exist <- intersect(integer_vars, names(df))
